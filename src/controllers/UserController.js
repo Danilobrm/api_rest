310bb58b86af78689d86240585b258a9d4dcda8a
danilobrm <danilobr.web@gmail.com>
2022-10-21 14:51:31 -0300

    commit api

---
+++ src/controllers/UserController.js
@@ -0,0 +1,81 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      // const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
+      console.log('USER ID', req.userId);
+      console.log('USER EMAIL', req.userEmail);
+
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+}
+
+export default new UserController();
---
+++ src/models/Aluno.js
@@ -0,0 +1,74 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Aluno extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 20],
+            msg: 'Nome precisa ter entre 3 e 20 caracteres.',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 20],
+            msg: 'Sobreome precisa ter entre 3 e 20 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'O email já está em uso.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'E-mail inválido.',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro.',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um número inteiro ou flutuante.',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um número inteiro ou flutuante.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: 'aluno_id' });
+  }
+}
